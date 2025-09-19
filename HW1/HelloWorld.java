f2912e0e5c312a86808a0bd499dfc9438f48a0cc
hoanghuhu <nayuta1606@gmail.com>
2025-09-19 23:26:57 +0700

    Introduction

---
+++ HW1/HelloWorld.java
@@ -0,0 +1,13 @@
+public class Introduction {
+    public static void main(String[] args){
+        System.out.println("Dương Hoàng Anh\t24022762\t2526I_INT2204_15\tglenn1606\tnayuta1606@gmail.com");
+        for(int i = 9; i >1 ; i--) {
+            System.out.println( i + " bottles of beer on the wall, "
+                    + i + " bottles of beer.\n" + "Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottles of beer.\n" + "Take one down, pass it around, \n");
+        System.out.println("No more bottles of beer on the wall.");
+
+    }
+
+}
